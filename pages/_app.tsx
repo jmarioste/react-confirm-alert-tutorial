edb7cfc4f3e5d66eb32a67a6273668c39b070958
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-02-10 16:33:38 +0800

    add comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,13 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import AlertProvider from "../components/alert/AlertProvider";
 import AlertDialog from "../components/alert/AlertDialog";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    // 👇 we should be able to specficy the AlertDialog we want to use
+    // whether it's implemented using @headlessui + tailwind or MUI or bootstrap
     <AlertProvider AlertComponent={AlertDialog}>
       <Component {...pageProps} />
     </AlertProvider>
   );
 }
-
 export default MyApp;
