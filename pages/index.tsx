c7a9177ec7b11e62bb01354101401059d17d09c1
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-02-10 16:33:56 +0800

    Remove Extra alert

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,22 +20,10 @@ const Home: NextPage = () => {
     });
   };
 
-  const handleClickAnother = () => {
-    showAlert({
-      title: "Confirm Subscription",
-      confirmMessage: "You will be subscribed for one year?",
-      async onConfirm() {
-        //simulates asynchronous API calls
-        await sleep(1000);
-        console.log("You have been subscribed");
-      },
-    });
-  };
   return (
     <div className="container mx-auto">
       <h1 className="text-4xl">NextJS + TailwindCSS Starter </h1>
       <button onClick={handleClick}>Show alert</button>
-      <button onClick={handleClickAnother}>Show another alert</button>
     </div>
   );
 };
